9ee2b690e451a6fbfad2c2582380524372522c00
Nguta <miriamnguta949@gmail.com>
2021-03-03 15:04:39 +0300

    first commit

---
+++ strings.py
@@ -0,0 +1,19 @@
+a = "strawberry "
+b = " tropical"
+#relational operators
+print (a > b)
+print (a < b)
+print (a == b)
+#concantenation
+print (a + b)
+
+print (a * 6)
+print ("This is Anne's workshop")
+x = "90"
+y = "70"
+z = x+y
+print (z)
+
+
+
+
